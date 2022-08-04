e22703ec9e25e94e16251e2a8cb8d475264ecb64
Keks <keks@htmlacademy.ru>
2022-08-04 16:23:19 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/app.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector('.main-nav__toggle');
+const menuList = document.querySelector('.main-nav__lists');
+
+menuButton.addEventListener('click', () => {
+  menuButton.classList.toggle('main-nav__toggle--open');
+  menuList.classList.toggle('main-nav__lists--open');
+});

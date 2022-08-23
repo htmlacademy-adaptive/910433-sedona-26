2dc2f41227fb48bb44275045a4380df820997712
alexeirechitskiy <slowspirit@gmail.com>
2022-08-23 18:36:41 +0300

    исправил ошибки: - добавил vertical-align: bottom для изображений на странице catalog.html; - добавил скрипт для открытия модальных окон

--- source/js/app.js
+++ source/js/app.js
@@ -1,13 +1,24 @@
-const menuButton = document.querySelector('.main-nav__toggle');
-const menuList = document.querySelector('.main-nav__lists');
 const pageBodyClass = document.querySelector('.page-body');
 
-const submitFormButton= document.querySelector('.form__button');
+//элементы меню
+const menuButton = document.querySelector('.main-nav__toggle');
+const menuList = document.querySelector('.main-nav__lists');
 
+//элементы формы
+const submitFormButton= document.querySelector('.form__button');
+const firstNameField = document.querySelector('#firstName');
+const secondNameField = document.querySelector('#secondName');
+const thirdNameField = document.querySelector('#thirdName');
+const phoneField = document.querySelector('#phone');
+const emailField = document.querySelector('#email');
+const textareaField = document.querySelector('#textarea');
+
+//элементы модального окна ОШИБКА
 const popupButtonError = document.querySelector('#popupButtonError');
 const popupWindowError = document.querySelector('#popupWindowError');
 const overlayError = document.querySelector('#overlayError');
 
+//элементы модального окна УСПЕХ
 const popupButtonSuccess = document.querySelector('#popupButtonSuccess');
 const popupWindowSuccess = document.querySelector('#popupWindowSuccess');
 const overlaySuccess = document.querySelector('#overlaySuccess');
@@ -20,30 +31,39 @@ menuButton.addEventListener('click', () => {
 
 // открытие модальных окон
 submitFormButton.addEventListener('click', () => {
-  const inputs = document.querySelectorAll('.form__input-field-required');
-  inputs.forEach(el => {
-    if (el.value  === '') {
-      popupWindowError.classList.add('popup--open');
-      overlayError.classList.add('overlay--open');
-      pageBodyClass.style.overflow = 'hidden';
-    }
-    else {
-      popupWindowSuccess.classList.add('popup--open');
-      overlaySuccess.classList.add('overlay--open');
-      pageBodyClass.style.overflow = 'hidden';
-    }
-  });
+  if (!firstNameField.value || !secondNameField.value || !phoneField.value || !emailField.value) {
+    popupWindowError.classList.add('popup--open');
+    overlayError.classList.add('overlay--open');
+    pageBodyClass.style.overflow = 'hidden';
+  } else {
+    popupWindowSuccess.classList.add('popup--open');
+    overlaySuccess.classList.add('overlay--open');
+    pageBodyClass.style.overflow = 'hidden';
+  }
 });
 
 // закрытие модальных окон
+// закрытие модального окна ОШИБКА
 popupButtonError.addEventListener('click', () => {
   popupWindowError.classList.remove('popup--open');
   overlayError.classList.remove('overlay--open');
   pageBodyClass.style.overflow = 'visible';
 });
 
+// закрытие модального окна УСПЕХ
 popupButtonSuccess.addEventListener('click', () => {
   popupWindowSuccess.classList.remove('popup--open');
   overlaySuccess.classList.remove('overlay--open');
   pageBodyClass.style.overflow = 'visible';
+  clearFields();
 });
+
+//функция очистки полей формы
+function clearFields() {
+  firstNameField.value = '';
+  secondNameField.value = '';
+  thirdNameField.value = '';
+  phoneField.value = '';
+  emailField.value = '';
+  textareaField.value = '';
+}

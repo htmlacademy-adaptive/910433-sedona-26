401123c68d28398bf38f363b99fab9c260f8c0cb
alexeirechitskiy <slowspirit@gmail.com>
2022-08-31 22:33:08 +0300

    исправил пункт Б13

--- source/js/app.js
+++ source/js/app.js
@@ -1,9 +1,9 @@
 const pageBodyClass = document.querySelector('.page-body');
 
 //элементы меню
-const menuButton = document.querySelector('.main-nav__toggle');
+const menuButton = document.querySelector('.main-header__toggle');
 const menu = document.querySelector('.main-nav');
-const menuList = document.querySelector('.main-nav__lists');
+const menuList = document.querySelector('.main-header__lists');
 
 //элементы формы
 const form = document.querySelector('.form');
@@ -36,7 +36,7 @@ const clearFields = () => {
 }
 
 //отображение меню, если не подгрузился JS
-menu.classList.remove('main-nav--no-js');
+menu.classList.remove('main-header__nav--no-js');
 
 // открытие меню
 menuButton.addEventListener('click', () => {
